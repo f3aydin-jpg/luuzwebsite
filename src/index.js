e5113e8d9648a04dd3ff61969b38407c2fc29c69
f3aydin-jpg <f.3aydin@gmail.com>
2026-01-20 00:00:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import WallArtShop from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WallArtShop /> 
   </React.StrictMode>
 );
