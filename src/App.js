7cc5d5b2991ff97c99b557611b5921469d9729ec
f3aydin-jpg <f.3aydin@gmail.com>
2026-01-31 20:10:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -679,9 +679,9 @@ export default function WallArtShop() {
     style={{ 
       fontFamily: "'Montserrat', sans-serif", 
       fontSize: 'clamp(1.5rem, 5vw, 2.5rem)', 
-      fontWeight: '400',           // En kalın seviyeye (Black) çıkardık
+      fontWeight: '600',           // En kalın seviyeye (Black) çıkardık
       textTransform: 'uppercase',  // Bütün harfleri BÜYÜK yaptık
-      letterSpacing: '-0.02em',    // Harfleri birbirine yaklaştırarak daha "bold" bir hava verdik
+      letterSpacing: '0.01em',    // Harfleri birbirine yaklaştırarak daha "bold" bir hava verdik
       lineHeight: '1.1'}}>Çok Satanlar</h3>
 </div>  
           <button 
